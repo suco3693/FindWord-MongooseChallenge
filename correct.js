d77a129de00c7353d39175feca107448eb91d378
Sutton <suco3693@gmail.com>
2020-07-21 15:31:47 -0700

    Finish of pullFirstUser, pullPaymentsForUsers, & convertToStr

--- correct.js
+++ correct.js
@@ -1,3 +1,53 @@
-/* 
+/*
  * Put the correct implementation of incorrect.js here.
  */
+
+const models = require('./models');
+
+/*
+ * Pulls the first user from the user id list that can be found in the database.
+ * If a particular user can't be found, ignore the id and look for the next user
+ * until one can be found.
+ */
+async function pullFirstUser(userIds) {
+    for (var idx = 0; idx < userIds.length; idx++) {
+        let user = await models.user.findOne({ _id: userIds[idx] });
+        if (user) {
+            return user;
+        }
+    }
+    return null;
+}
+module.exports.pullFirstUser = pullFirstUser;
+
+/*
+ * Processes the sorted array and returns the results in
+ * the same order as the input documents (e.g., the
+ * result of the first element in the argument array should
+ * be stored in the first element of the result array, and so on...).
+ */
+async function pullPaymentsForUsers(users) {
+    let result = [];
+    if (!users) {
+        return result;
+    }
+    for (let user of users) {
+        let payments = await models.payment.find({ user: user });
+        result.push(payments.map((payment) => payment._id));
+    }
+    return result; // array of array with payments (the first array should contain payments for the first user)
+}
+module.exports.pullPaymentsForUsers = pullPaymentsForUsers;
+
+/*
+ * Converts the number to a string (return nothing if
+ * something other than a number is passed to the function)
+ */
+function convertToStr(num) {
+    if (typeof num !== 'number') {
+        return;
+    } else {
+        return num.toString();
+    }
+}
+module.exports.convertToStr = convertToStr;
--- incorrect.js
+++ incorrect.js
@@ -1,15 +1,14 @@
-/* This file contains a number of issues. Your task is to fix the issues 
+/* This file contains a number of issues. Your task is to fix the issues
  * while retaining the intended functionality.
- * 
+ *
  * You should feel free to import libraries if they will help you make
  * the code simpler (no point in reinventing the wheel!)
- * 
+ *
  * Also, don't hesitate to ask if you have any questions about the intended
  * functionality of any of the functions below.
  */
 
-const
-	models = require("./models");
+const models = require('./models');
 
 /*
  * Pulls the first user from the user id list that can be found in the database.
@@ -17,36 +16,37 @@ const
  * until one can be found.
  */
 function pullFirstUser(userIds) {
-	return userIds.forEach(userId => {
-		let user = models.user.findOne({_id: userId});
-		if (user) return user;
-	});
+    return userIds.forEach((userId) => {
+        let user = models.user.findOne({ _id: userId });
+        if (user) return user;
+    });
 }
 module.exports.pullFirstUser = pullFirstUser;
 
 /*
  * Processes the sorted array and returns the results in
- * the same order as the input documents (e.g., the 
+ * the same order as the input documents (e.g., the
  * result of the first element in the argument array should
  * be stored in the first element of the result array, and so on...).
  */
 function pullPaymentsForUsers(users) {
-	let result = [];
-	for (let user of users) {
-		models.payment.findOne({user_id: user._id}).then(payments => {
-			result.push(payments);
-		});
-	}
-	return result; // array of array with payments (the first array should contain payments for the first user)
+    let result = [];
+    for (let user of users) {
+        //probably find all
+        models.payment.findOne({ user_id: user._id }).then((payments) => {
+            result.push(payments);
+        });
+    }
+    return result; // array of array with payments (the first array should contain payments for the first user)
 }
 module.exports.pullPaymentsForUsers = pullPaymentsForUsers;
 
 /*
- * Converts the number to a string (return nothing if 
+ * Converts the number to a string (return nothing if
  * something other than a number is passed to the function)
  */
 function convertToStr(num) {
-	if (!num) return num.toString();
+    if (!num) return num.toString();
 }
 module.exports.convertToStr = convertToStr;
 
@@ -56,24 +56,27 @@ module.exports.convertToStr = convertToStr;
  * Sometimes the payment id might not match a payment.
  */
 function getPaymentWithUser(paymentId) {
-	let payment = models.payment.find({_id: paymentId});
-	payment.user = models.user.find({_id: payment.user});
-	return payment;
+    let payment = models.payment.find({ _id: paymentId });
+    payment.user = models.user.find({ _id: payment.user });
+    return payment;
 }
 module.exports.getPaymentWithUser = getPaymentWithUser;
 
 /*
- * Pulls all active payments for the users and retuns an object
+ * Pulls all active payments for the users and returns an object
  * mapping the user id to the user's payments (string to array).
  * Note: userIds is passed in as an array of strings
+ *
+ * Note: Active
  */
 function getGroupedUserPmts(userIds) {
-	let result = {};
-	let payments = models.payment.find({user_id: userIds});
-	userIds.forEach(userId => {
-		payments = payments.filter(payment => userIds.includes(payment.user_id));
-		result[userId] = payments;
-	});
-	return result;
+    let result = {};
+    let payments = models.payment.find({ user_id: userIds });
+    userIds.forEach((userId) => {
+        payments = payments.filter((payment) => userIds.includes(payment.user_id));
+        //need to push is already there
+        result[userId] = payments;
+    });
+    return result;
 }
 module.exports.getGroupedUserPmts = getGroupedUserPmts;
